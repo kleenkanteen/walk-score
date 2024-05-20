28aeeed13d2939f9e10e8edde4961ed8b4eefac4
kleenkanteen <antpalmolive@gmail.com>
2024-05-20 05:20:26 -0400

    feat: initialize nuxt with maptiler map

---
+++ components/Map.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="map-wrap">
+      <a href="https://www.maptiler.com" class="watermark"><img
+          src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo"/></a>
+      <div class="map" ref="mapContainer"></div>
+    </div>
+  </template>
+  
+  <script>
+  import { shallowRef, onMounted, onUnmounted, markRaw } from 'vue';
+  import { Map, NavigationControl } from 'maplibre-gl';
+  
+  export default {
+    name: "Map",
+    setup () {
+      const mapContainer = shallowRef(null);
+      const map = shallowRef(null);
+  
+      onMounted(() => {
+        const apiKey = 'YOUR_KEY';
+  
+        const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+  
+        map.value = markRaw(new Map({
+          container: mapContainer.value,
+          style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${apiKey}`,
+          center: [initialState.lng, initialState.lat],
+          zoom: initialState.zoom
+        }));
+
+        map.value.addControl(new NavigationControl(), 'top-right');
+  
+      }),
+      onUnmounted(() => {
+        map.value?.remove();
+      })
+  
+      return {
+        map, mapContainer
+      };
+    }
+  };
+  </script>
+  
+  
+  <style scoped>
+  @import 'maplibre-gl/dist/maplibre-gl.css';
+  
+  .map-wrap {
+    position: relative;
+    width: 100%;
+    height: calc(100vh - 77px); /* calculate height of the screen minus the heading */
+  }
+  
+  .map {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+  
+  .watermark {
+    position: absolute;
+    left: 10px;
+    bottom: 10px;
+    z-index: 999;
+  }
+  </style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app">
+    <Navbar />
+    <Map />
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+import Map from '../components/Map.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Map
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+
+.app {
+  text-align: center;
+}
+</style>
