33e0f78f3cdaba3fa6c146cb2a93ed331773628a
wenxpan <panwenxuan42@gmail.com>
2024-05-22 19:28:19 +1000

    add nuxtui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxthub/core', "@nuxt/eslint", "@nuxtjs/tailwindcss"],
+  modules: [
+    '@nuxthub/core',
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/ui",
+    "nuxt-icon",
+    "@nuxtjs/color-mode"
+  ],
+  colorMode: {
+    preference: 'system', // default theme
+    dataValue: 'theme', // activate data-theme in <html> tag
+    classSuffix: '',
+  },
+  tailwindcss: { exposeConfig: true },
   hub: {
     database: true,
     kv: true,
